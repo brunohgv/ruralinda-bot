a931fe99a36f13f14b1d9070b51bc30f37dc63c1
Bruno Vasconcelos <brunohgv2@gmail.com>
2019-03-31 15:49:30 -0300

    Implementation of 'ajuda' command

--- index.js
+++ index.js
@@ -11,8 +11,17 @@ client.on('ready', () => {
 
 client.on('message', msg => {
   if (msg.content === '!sobre') {
-    msg.reply(`oi, eu sou a ${CONSTANTS.botName}!\nSaiba mais sobre mim em ${CONSTANTS.botRepo}. ;)`)
+    msg.reply(`oi, eu sou a ${CONSTANTS.botName}!\nSaiba mais sobre mim em ${CONSTANTS.botRepo}.)`)
   }
-});
+  if (msg.content === '!ajuda') {
+    let commands = require('./documentation/help.json')
+    let responseMessage = '```\n'
+    commands.forEach(command => {
+      responseMessage += (`${command.command}: ${command.description}\n\n`)
+    })
+    responseMessage += '```'
+    msg.reply(responseMessage)
+  }
+})
 
 client.login(process.env.APP_TOKEN)
